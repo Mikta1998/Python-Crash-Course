a36a3412e595d5f5701d198984b32e15ffa536b3
hsan.filip.timov <timov20168@hs-ansbach.de>
2025-08-30 18:18:58 +0200

    new changes

--- foods.py
+++ foods.py
@@ -1,6 +1,9 @@
 my_foods = ["pizza", "falafel", "carrot cake"]
 friend_foods = my_foods[:]
 
+my_foods.append("hamburger")
+friend_foods.append("cake")
+
 print(f"My favorite foods are:")
 print(my_foods)
 
