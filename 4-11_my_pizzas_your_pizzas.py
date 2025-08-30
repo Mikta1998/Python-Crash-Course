2479921790b84bb0a5b9cbb7be59a5a17ce08275
hsan.filip.timov <timov20168@hs-ansbach.de>
2025-08-30 18:24:14 +0200

    new changes

---
+++ 4-11_my_pizzas_your_pizzas.py
@@ -0,0 +1,8 @@
+my_pizzas = ["margarita", "vezuvio", "capricioza"]
+your_pizzas = my_pizzas[:]
+
+my_pizzas.append("speziale")
+your_pizzas.append("vier käse")
+
+print(my_pizzas)
+print(your_pizzas)
