7da928a6b318ef851dfe0cade22b73282bc32678
Filip.Timov <timov20168@hs-ansbachde>
2025-08-30 17:03:31 +0200

    new changes

---
+++ 4-1_pizzas.py
@@ -0,0 +1,4 @@
+pizzas = ["margarita", "vezuvio", "speziale"]
+for pizza in pizzas:
+    print(f"I like {pizza.title()}!")
+print(f"I really like pizza!")
---
+++ first_numbers.py
@@ -0,0 +1,11 @@
+for value in range(1,5):
+    print(value)
+
+print(f"\n")
+
+for value in range(6):
+    print(value)
+
+
+numbers = list(range(1,6))
+print(numbers)
